b99d59417142ff30c56db41b0b8652460a71d6d2
noahbearx3 <noah.b3rkhout@gmail.com>
2020-10-01 15:59:53 +0200

    Crosshair :)

--- Assets/Scripts/WASDMove.cs
+++ Assets/Scripts/WASDMove.cs
@@ -6,7 +6,7 @@ public class WASDMove : MonoBehaviour
 {
     //2D Vectors made to store player poisiton and mouse postion
     Vector2 playerMovement;
-    Vector2 mousePos;
+    
 
     //Speed in wihch player will move 
     public float speed = 20f;
@@ -32,7 +32,7 @@ public class WASDMove : MonoBehaviour
          playerMovement.x = Input.GetAxisRaw("Horizontal");
          playerMovement.y = Input.GetAxisRaw("Vertical");
 
-         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+         
  
      }
 
@@ -40,9 +40,7 @@ public class WASDMove : MonoBehaviour
         // Using positoins from update function this line of code will enable the player to move
          rb.MovePosition(rb.position + playerMovement * speed * Time.fixedDeltaTime);
         // Calculate the position the player wants to look in by subtracting the 2 vectors mouse position and player position
-         Vector2 lookDir = mousePos - rb.position;
-        // Using Atan to calculate the x-axis to the directional vector using the lookDir, The player can look around with the Mouse, Atan is convereted from Radians to Degrees
-         float viewAngle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - 90f;
-         rb.rotation = viewAngle;
+         
+       
      }
 }
