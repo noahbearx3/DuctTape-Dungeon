3e926726ea147991f67b2b8af054245b02c2e2b5
Sylvkers <sylv.kers@gmail.com>
2020-10-21 13:28:32 +0200

    Range duck will go in attack animation if the distance is 3 or below ;d

---
+++ Assets/Scripts/RangeDuckController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeDuckController : MonoBehaviour
+{
+    public GameObject mainPlayer;
+    Animator attackAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainPlayer = GameObject.FindGameObjectWithTag("Player");
+        attackAnim = gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Mathf.Round(Vector3.Distance(mainPlayer.transform.position, transform.position));
+        print("Distance to other: " + dist);
+
+        if (dist <= 3)
+        {
+            attackAnim.SetTrigger("Attack");
+            attackAnim.ResetTrigger("Idle");
+        } else
+        {
+            attackAnim.SetTrigger("Idle");
+            attackAnim.ResetTrigger("Attack");
+        }
+
+    }
+}
