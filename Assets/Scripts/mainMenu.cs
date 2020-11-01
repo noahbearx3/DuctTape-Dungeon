fc130aaa88d435f03335c2259e05929867be1c11
Sylvkers <sylv.kers@gmail.com>
2020-11-01 19:04:38 +0100

    Start screen + Script

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+
+    public void OnStartGame()
+    {
+        int index = Random.Range(1, 3);
+        SceneManager.LoadScene(index); 
+    }
+
+
+    public void OnExitGame()
+    {
+        Application.Quit();
+    }
+}
