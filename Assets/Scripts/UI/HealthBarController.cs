7ea93cf8345583eabab53b8893fb3e50de6460e2
noahbearx3 <noah.b3rkhout@gmail.com>
2020-11-02 17:40:08 +0100

    Healthbar and clean

---
+++ Assets/Scripts/UI/HealthBarController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarController : MonoBehaviour
+{
+    
+    private Transform healthBar;
+    
+
+    // Find the bar gameObject using a tag
+    void Start()
+    {
+         healthBar = transform.Find("Bar");
+        
+       
+    }
+
+    //Used to set the size of the health bar
+    public void SetSize(float sizeNormalized){
+        healthBar.localScale = new Vector3 (sizeNormalized,1f);
+    }
+
+}
+    // Update is called once per frame
+    
---
+++ Assets/Scripts/UI/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+
+    [SerializeField] private HealthBarController bar;
+    // Start is called before the first frame update
+     void Start()
+    {
+     StartCoroutine("DoCheck");
+    }
+
+    
+    void Update()
+    {
+       
+    }
+
+    IEnumerator DoCheck() {
+    float health =1f;
+     for(;;) {
+         if(health>0){
+             health -= .01f;
+             bar.SetSize(health);
+         }
+         yield return new WaitForSeconds(.1f);
+     }
+    }
+}
