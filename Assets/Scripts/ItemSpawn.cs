4a3c6b274fb82768a5e9c25f1ce02ba7a6d09f6b
Sylvkers <sylv.kers@gmail.com>
2020-11-03 18:38:37 +0100

    Random Item from chest

---
+++ Assets/Scripts/ItemSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour
+{
+    public List<GameObject> Items = new List<GameObject>();
+    private int randomItem;
+
+    public GameObject mainPlayer;
+
+    private void Start()
+    {
+        mainPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        float dist = Vector3.Distance(transform.position, mainPlayer.transform.position);
+
+        if (dist <= 1.5 && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(gameObject);
+
+            // Generates a random Item from the list
+            randomItem = Random.Range(0, Items.Count);
+
+            Instantiate(Items[randomItem], transform.position, Quaternion.identity);
+        
+        }
+    }
+}
