3cabde348fac23bb5f39dfac163be317ccf5128b
Sylvkers <sylv.kers@gmail.com>
2020-11-04 16:26:25 +0100

    Open/Close note on wall  + test note

---
+++ Assets/Scripts/OpenNote.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenNote : MonoBehaviour
+{
+    public GameObject mainPlayer;
+    bool reading = false;
+    public Image note;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainPlayer = GameObject.FindGameObjectWithTag("Player");
+        note.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(transform.position, mainPlayer.transform.position);
+
+        if (dist <= 1.5)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (reading)
+                {
+                    reading = false;
+                    note.enabled = false;
+                }
+                else
+                {
+                    reading = true;
+                    note.enabled = true;
+                }
+
+            }
+        }
+        else
+        {
+            reading = false;
+            note.enabled = false;
+        }
+    }
+}
