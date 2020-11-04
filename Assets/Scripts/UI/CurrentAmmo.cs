0d14ad91cb5e288906aa332bc9d368477fa2e510
noahbearx3 <noah.b3rkhout@gmail.com>
2020-11-04 12:14:46 +0100

    UI Ammo attempt

---
+++ Assets/Scripts/UI/CurrentAmmo.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentAmmo : MonoBehaviour
+{
+     // Sprites to be added in editor
+    public Sprite bulletSprite;
+    public Sprite iceBulletSprite;
+    public Sprite lightningBulletSprite;
+    public Sprite fireBulletSprite;
+
+    private bool bulletUI = true;    
+    private bool iceBulletUI;
+    private bool fireBulletUI;
+    private bool lightningBulletUI;
+
+    public Image ammoImg;
+    
+
+    // GameObject holding script to be added in inspector in order to access current weapon
+    public PlayerController items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         // Get image component in order to change UI
+         ammoImg = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Call CheckAmmo Function once per frame
+        CheckAmmo();
+    }
+
+    void CheckAmmo(){
+    // Call the PlayerController script once per frame to check the booleans state constantly to see current ammo 
+        //bulletUI = items.bulletNormal;
+        fireBulletUI = items.emberPicked;
+        iceBulletUI = items.icePicked;
+        lightningBulletUI = items.batteryPicked;
+        SortAmmo();
+    //If functions used to check current ammo and then change sprite depending on result
+        if(bulletUI == true && fireBulletUI == false && iceBulletUI == false && lightningBulletUI == false){
+            ammoImg.sprite = bulletSprite;
+        }
+        if(bulletUI ==false  && fireBulletUI == true && iceBulletUI == false && lightningBulletUI == false){
+            ammoImg.sprite = fireBulletSprite;
+        }
+        if(bulletUI == false && fireBulletUI == false && iceBulletUI == true && lightningBulletUI == false){
+            ammoImg.sprite = iceBulletSprite;
+        }
+        if(bulletUI == false && fireBulletUI == false && iceBulletUI == false && lightningBulletUI == true){
+            ammoImg.sprite = lightningBulletSprite;
+        }
+    }
+
+    void SortAmmo(){
+        if(fireBulletUI == true || iceBulletUI == true ||lightningBulletUI == true){
+            bulletUI = true;
+        }
+    }
+}
--- Assets/Scripts/UI/CurrentWeapon.cs
+++ Assets/Scripts/UI/CurrentWeapon.cs
@@ -9,10 +9,7 @@ public class CurrentWeapon : MonoBehaviour
     public Sprite pistolSprite;
     public Sprite ak47Sprite;
     public Sprite shottieSprite;
-    public Sprite bulletSprite;
-    public Sprite iceBulletSprite;
-    public Sprite lightningBulletSprite;
-    public Sprite fireBulletSprite;
+   
 
     // GameObject holding script to be added in inspector in order to access current weapon
     public CrosshairAimPlusShoot bools;
@@ -22,9 +19,7 @@ public class CurrentWeapon : MonoBehaviour
     private bool pistolUI;    
     private bool ak47UI;
     private bool shotgunUI;
-    private bool bulletUI;    
-    private bool iceBulletUI;
-    private bool fireBulletUI;
+    
 
 
     public Image img;
@@ -43,8 +38,6 @@ public class CurrentWeapon : MonoBehaviour
     {   
         //Call CheckWeapon Function once per frame
         checkWeapon();
-        //Call CheckAmmo Function once per frame
-        CheckAmmo();
     }
     
     //Function is used to check current weapon
@@ -62,31 +55,6 @@ public class CurrentWeapon : MonoBehaviour
         }
         if(pistolUI == false && ak47UI == false && shotgunUI == true){
             img.sprite = shottieSprite;
-        }
-    }
-
-    void CheckAmmo(){
-    // Call the PlayerController script once per frame to check the booleans state constantly to see current ammo 
-        //bulletUI = items.bulletNormal;
-        fireBulletUI = items.emberPicked;
-        iceBulletUI = items.icePicked;
-        lightningBulletUI = items.batteryPicked;
-        SortAmmo();
-    //If functions used to check current ammo and then change sprite depending on result
-        if(fireBulletUI == true && iceBulletUI == false && lightningBulletUI == false){
-            img.sprite = pistolSprite;
-        }
-        if(fireBulletUI == true && iceBulletUI == false && lightningBulletUI == false){
-            img.sprite = ak47Sprite;
-        }
-        if(fireBulletUI == true && iceBulletUI == false && lightningBulletUI == false){
-            img.sprite = shottieSprite;
-        }
-    }
-
-    void SortAmmo(){
-        if(fireBulletUI == true && iceBulletUI == false && shotgunUI== false){
-             img.sprite = pistolSprite;
         }
     }
 }
