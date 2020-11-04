920fe326c61d9a59ce0b0358319bcf95eed77c1f
Sylvkers <sylv.kers@gmail.com>
2020-11-04 20:21:43 +0100

    Computer & Door

---
+++ Assets/Scripts/ComputerCode.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputerCode : MonoBehaviour
+{
+    public GameObject mainPlayer;
+    bool reading = false;
+    public Image note;
+    public bool codeReceived;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainPlayer = GameObject.FindGameObjectWithTag("Player");
+        note.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(transform.position, mainPlayer.transform.position);
+
+        if (dist <= 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (reading)
+                {
+                    reading = false;
+                    note.enabled = false;
+                }
+                else
+                {
+                    reading = true;
+                    note.enabled = true;
+                    codeReceived = true;
+                }
+
+            }
+        }
+        else
+        {
+            reading = false;
+            note.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/doorOpen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpen : MonoBehaviour
+{
+
+    public GameObject mainPlayer;
+    public GameObject computer;
+    Animator openDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainPlayer = GameObject.FindGameObjectWithTag("Player");
+        openDoor = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ComputerCode computerScript = computer.GetComponent<ComputerCode>();
+        float dist = Vector3.Distance(transform.position, mainPlayer.transform.position);
+
+        if (dist <= 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if(computerScript.codeReceived == true)
+                {
+                    openDoor.SetTrigger("CodeReceived");
+                    Physics2D.IgnoreCollision(mainPlayer.transform.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+                }
+
+            }
+        }
+    }
+}
